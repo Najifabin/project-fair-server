04ae331a26660e47a1693135d2618cbf0f061678
Najifabin <najifabin2001@gmail.com>
2024-12-11 23:16:56 +0530

    first commit

---
+++ controllers/userController.js
@@ -0,0 +1,41 @@
+const users = require('../models/userModel')
+
+// register
+exports.registerController = async (req,res)=>{
+    console.log("Inside register controller");
+    const {username,email,password} =req.body
+    console.log(username,email,password);
+    try{
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            res.status(406).json("user already exist..Please login")
+        }else{
+            const newUser = new users({
+                username,email,password,github:"",linkedin:"",profilePic:""
+            })
+            await newUser.save()
+            res.status(200).json(newUser)
+        }
+    }catch(err){
+        res.status(401).json(err)
+    }
+} 
+
+// login
+exports.loginController = async (req,res)=>{
+    console.log("Inside login controller");
+    const {email,password} =req.body
+    console.log(email,password);
+    try{
+        const existingUser = await users.findOne({email,password})
+        if(existingUser){
+            res.status(200).json({user:existingUser})
+        }else{
+            res.status(404).json("Invalid email / password")
+        }
+    }catch(err){
+        res.status(401).json(err)
+    }
+}
+
+// profile updation
---
+++ routes/router.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const userController = require('../controllers/userController')
+
+const router = new express.Router()
+
+// register -post
+router.post('/register',userController.registerController)
+// login -post
+router.post('/login',userController.loginController)
+module.exports = router
